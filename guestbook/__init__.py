2941ffeac12b79dfabbf2520a8c065c88f2740b2
Mario Campos <mcampos@anl.gov>
2016-03-21 13:31:27 -0500

    change -i default from 'localhost' to 'lo' as 'lo' is standard across linux

--- guestbook/__init__.py
+++ guestbook/__init__.py
@@ -49,7 +49,7 @@ def datetime_fmt_filter(dt):
 
 
 @click.command()
-@click.option('--interface', '-i', default='localhost', help='run server on this interface [localhost]')
+@click.option('--interface', '-i', default='lo', help='run server on this interface [lo]')
 @click.option('--port', '-p', default=8000, type=click.IntRange(1, 65535), help='run server on this TCP port [8000]')
 @click.option('--debug', '-d', default=False, is_flag=True, help='run server in debug mode')
 def main(interface, port, debug):
