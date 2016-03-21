3abf1300e5226dcc23abb19b6f9b37e24c8bc6f2
Mario Campos <mcampos@anl.gov>
2016-03-21 15:00:43 -0500

    fix license classifier for pypi

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   classifiers=[
       'Development Status :: 5 - Production/Stable',
       'Framework :: Flask',
-      'License :: MIT',
+      'License :: OSI Approved :: MIT License',
       'Programming Language :: Python',
       'Programming Language :: Python :: 2.6',
       'Programming Language :: Python :: 2.7',
