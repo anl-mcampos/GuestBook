fd11fdd3e1fd76172e8c88b0450eb41a2d442e25
Mario Campos <mcampos@anl.gov>
2016-03-21 10:33:04 -0500

    with the idiomatic 'with' instead of the open/close combo
    ProTip: use it with 'contextlib.closing()' so that it is portable across Python 2 and 3!

--- guestbook/__init__.py
+++ guestbook/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import shelve
-
+from contextlib import closing
 from datetime import datetime
 
 from flask import Flask, request, render_template, redirect, escape, Markup
@@ -11,24 +11,22 @@ DATA_FILE = 'guestbook.dat'
 
 
 def save_data(name, comment, create_at):
-    database = shelve.open(DATA_FILE)
-    if 'greeting_list' not in database:
-        greeting_list = []
-    else:
-        greeting_list = database['greeting_list']
-    greeting_list.insert(0, {
-        'name':      name,
-        'comment':   comment,
-        'create_at': create_at
-    })
-    database['greeting_list'] = greeting_list
-    database.close()
+    with closing(shelve.open(DATA_FILE)) as database:
+        if 'greeting_list' not in database:
+            greeting_list = []
+        else:
+            greeting_list = database['greeting_list']
+        greeting_list.insert(0, {
+            'name':      name,
+            'comment':   comment,
+            'create_at': create_at
+        })
+        database['greeting_list'] = greeting_list
 
 
 def load_data():
-    database = shelve.open(DATA_FILE)
-    greeting_list = database.get('greeting_list', [])
-    database.close()
+    with closing(shelve.open(DATA_FILE)) as database:
+        greeting_list = database.get('greeting_list', [])
     return greeting_list
 
 
